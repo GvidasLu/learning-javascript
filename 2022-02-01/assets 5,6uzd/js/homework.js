b41095a3b392ce077362d161caa4cb7afd07983a
GvidasLu <luspinis.gvidas@gmail.com>
2022-02-06 22:46:02 +0200

    hw update

--- 2022-02-01/assets 5,6uzd/js/homework.js
+++ 2022-02-01/assets 5,6uzd/js/homework.js
@@ -32,4 +32,35 @@ function chooseColor (x) {
 
 // 7 uzduotis
 
+function randomSkaicius(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+let zvakes = randomSkaicius(5, 3000);
+document.getElementById("skaiciai").innerHTML = zvakes;
+if (zvakes > 1000) {
+  let suma = zvakes - (zvakes * 3) / 100;
+  document.getElementsByClassName("suma")[0].innerHTML = suma;
+}
+
+function randomSkaicius(min, max) {
+    let zvakes = Math.floor(Math.random() * (max - min + 1) + min);
+    document.getElementById("skaiciai").innerHTML = zvakes;
+    if (zvakes > 1000) {
+        let suma = zvakes - (zvakes * 3) / 100;
+        document.getElementsByClassName("suma")[0].innerHTML = suma;
+      }
+      if (zvakes > 2000) {
+        let suma = zvakes - (zvakes * 4) / 100;
+        document.getElementsByClassName("suma")[0].innerHTML = suma;
+      }
+
+  }
+function bendras(){
+randomSkaicius(5, 3000); sugeneruoti(1, 30);
+}
+  const mygtukas = document.getElementsByTagName('button')[0]
+mygtukas.addEventListener('click', ()=> bendras());
+
+
+// 9 uzduotis
 
